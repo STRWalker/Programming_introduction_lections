27318587371852f336877332204cb676e376ee06
Some_body <some_body@gmail.com>
2022-12-21 17:48:04 +0300

    Add random

--- lection1/3_sum/Program.cs
+++ lection1/3_sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int number_a = 3;
-int number_b = 5;
+﻿int number_a = new Random().Next(1, 10);
+Console.WriteLine("Первое число: " + number_a);
+int number_b = new Random().Next(1, 10);
+Console.WriteLine("Второе число: " + number_b);
 int result = number_a + number_b;
-Console.WriteLine(result);
+Console.WriteLine("Сумма: " + result);
