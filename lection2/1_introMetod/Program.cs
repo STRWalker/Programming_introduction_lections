25e697087bacf7f707b9ad88f34ffe1ad401aa1a
Some_body <some_body@gmail.com>
2022-12-23 17:48:02 +0300

    Solve first task

--- lection2/1_introMetod/Program.cs
+++ lection2/1_introMetod/Program.cs
@@ -1,4 +1,22 @@
 ﻿/* Лекция 2.
-   Задание 1: */
-   
-Console.WriteLine("Hello, World!");
+   Задание 1: Найти максимальное число из 9*/
+
+int num1 = 4, num2 = 7, num3 = 1,
+    num4 = -4, num5 = 31, num6 = 8,
+    num7 = 31, num8 = 2, num9 = 0;
+
+// Нахождение максимального числа из трёх
+int Max(int number1, int number2, int number3) {
+    int max_number = number1;
+    if (number2 > max_number) max_number = number2;
+    if (number3 > max_number) max_number = number3;
+    return max_number;
+}
+
+int max = Max(
+    Max(num1, num2, num3),
+    Max(num4, num5, num6),
+    Max(num7, num8, num9)
+);
+
+Console.WriteLine($"max = {max}");
